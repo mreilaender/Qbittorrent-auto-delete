b659daaed185ee8b9d86bbf098199c46dcdf7d1c
Manuel Reiländer <manuel.reilaender@compax.at>
2026-08-23 19:29:23 +0200

    add logging for removing torrent with test-mode on

--- src/main/python/qbittorrent_auto_delete/torrent_utils.py
+++ src/main/python/qbittorrent_auto_delete/torrent_utils.py
@@ -501,8 +501,7 @@ def remove_torrents_by_space(torrents: List[Dict[str, Any]], categories_space: L
             continue
 
         if test_mode:
-            print(torrent)
-            logger.info("%s ", test_mode)
+            logger.info("[test-mode=%s] Removing torrent with path [%s]", test_mode, torrent['content_path'])
 
         space_freed += torrent['size'] / BYTES_TO_GB
         torrents_removed_info.append(_build_torrent_info(torrent))
